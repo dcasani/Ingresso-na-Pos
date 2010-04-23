2282662b0c2765e3f5b730943f17af1eaf2d6256
Silvia Esparrachiari <silviaeg@ime.usp.br>
2010-04-23 13:33:00 -0300

    Resolvidos testes com erros no modelo de Usuário (User). Adicionados testes de usuário para dados do endereço.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,23 @@ class User < ActiveRecord::Base
   validates_presence_of :nome_completo, :identidade, :data_de_nascimento,
                         :identidade, :tipo, :nacionalidade, :logradouro_permanente,
                         :numero_permanente, :cidade_permanente, :estado_permanente,
-                        :pais_permanente, :logradouro_correspondencia, 
+                        :pais_permanente, :logradouro_correspondencia,
                         :numero_correspondencia, :cidade_correspondencia,
                         :estado_correspondencia, :pais_correspondencia,
                         :email, :formacao_superior_graduacao , :message => ": Preencher campo obrigatório"
 
+                      # Campos de texto formados apenas por palavras (sem números nem caracteres especiais)
   validates_format_of :nome_completo, :tipo, :nacionalidade, :cidade_permanente,
                       :estado_permanente, :pais_permanente, :cidade_correspondencia,
                       :estado_correspondencia, :pais_correspondencia, :formacao_superior_graduacao,
-                      :with => /\A[a-zA-Z][a-zA-Z\s]+[a-zA-Z]\z/, :message => ": Usar somente letras e espaços"
+                      :with => /\A[A-Za-zãàáâäèéêëìíîïõòóôöùúûüçÃÀÁÂÄÈÉÊËÌÍÎÏÖÒÓÔÙÚÛÜÇ]+( [A-Za-zãàáâäèéêëìíîïõòóôöùúûüçÃÀÁÂÄÈÉÊËÌÍÎÏÖÒÓÔÙÚÛÜÇ]+)*\Z/, :message => ": Usar somente letras e espaços"
+
+                      # Campos formados por palavras e sinais de pontuação (.,-)
+  validates_format_of :logradouro_permanente, :logradouro_correspondencia,
+                      :with => /\A[A-Za-zãàáâäèéêëìíîïõòóôöùúûüçÃÀÁÂÄÈÉÊËÌÍÎÏÖÒÓÔÙÚÛÜÇ]+( [-,.]? ?[A-Za-zãàáâäèéêëìíîïõòóôöùúûüçÃÀÁÂÄÈÉÊËÌÍÎÏÖÒÓÔÙÚÛÜÇ]+)*\Z/, :message => ": Usar somente letras, espaços, vírgulas e traços"
+
+  validates_format_of :numero_permanente, :numero_correspondencia,
+                      :with => /\A[A-Za-z0-9]+\Z/, :message => ": Usar somente números e letras, sem espaços."
 
   #validates_format_of :email, :with => /^[-_a-z0-9]+(\\.[-_a-z0-9]+)*\\@([-a-z0-9]+\\.)*([a-z]{2,4})$/, :message => ": Formato de email inválido"
   #validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i , :message => ": Formato de email inválido"
@@ -20,8 +28,5 @@ class User < ActiveRecord::Base
   #validate_length_of :nome_completo, :in => 2..150
 
   validates_uniqueness_of :identidade
-
-  validates_numericality_of :numero_permanente, :numero_correspondencia
-  
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe User do
 
-  context "Validaçoes:" do
+  context "Validaçoes de nome:" do
 
     it "Deve validar um nome real" do
-      @user = User.new(:nome_completo => 'George Daniel Casanbi Delgado')
+      @user = User.new(:nome_completo => 'George Daniel Casani Delgado')
       @user.should have(:no).errors_on(:nome_completo)
     end
 
@@ -27,7 +27,7 @@ describe User do
 
     it "Não deve validar um nome real com espaço no final" do
      # pending "Nomes com espaços nao estão passando"
-      @user = User.new(:nome_completo => 'George Daniel Casanbi delgado ')
+      @user = User.new(:nome_completo => 'George Daniel Casanbi Delgado     ')
       @user.should have(1).errors_on(:nome_completo)
     end
 
@@ -35,7 +35,56 @@ describe User do
       @user = User.new(:nome_completo => 'George2Daniel')
       @user.should have(1).errors_on(:nome_completo)
     end
-    
+
+    it "Não deve validar um nome com caracteres estranhos" do
+      @user = User.new(:nome_completo => '!@#$%^&*()-+\|=-~`[]{}')
+      @user.should have(1).errors_on(:nome_completo)
+    end
+
+    it "Não deve validar um nome em branco" do
+      @user = User.new(:nome_completo => '')
+      @user.should have_at_least(1).errors_on(:nome_completo)
+    end
+
   end
-  
+
+  context "Validaçoes de endereço:" do
+
+    it "Não deve validar um logradouro permanente vazio" do
+      @user = User.new(:logradouro_permanente => '')
+      @user.should have_at_least(1).errors_on(:logradouro_permanente)
+    end
+
+    it "Não deve validar um logradouro permanente com caracteres estranhos" do
+      @user = User.new(:logradouro_permanente => '@$%&*()-+')
+      @user.should have_at_least(1).errors_on(:logradouro_permanente)
+    end
+
+    it "Não deve validar um número permanente vazio" do
+      @user = User.new(:numero_permanente => '')
+      @user.should have_at_least(1).errors_on(:numero_permanente)
+    end
+
+    it "Deve validar um número permanente que seja formado por números e letras" do
+      @user = User.new(:numero_permanente => 'b')
+      @user.should have(:no).errors_on(:numero_permanente)
+    end
+
+    it "Não deve validar uma cidade permanente vazio" do
+      @user = User.new(:cidade_permanente => '')
+      @user.should have_at_least(1).errors_on(:cidade_permanente)
+    end
+
+    it "Não deve validar um estado permanente vazio" do
+      @user = User.new(:estado_permanente => '')
+      @user.should have_at_least(1).errors_on(:estado_permanente)
+    end
+
+    it "Não deve validar um país permanente vazio" do
+      @user = User.new(:pais_permanente => '')
+      @user.should have_at_least(1).errors_on(:pais_permanente)
+    end
+
+  end
+
 end
