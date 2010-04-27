65291a60757a9110165b23684ae1185aa74fafe4
Silvia Esparrachiari <silviaeg@ime.usp.br>
2010-04-27 15:19:50 -0300

    Atualizado modelo para ignorar campos não obrigatórios do usuário.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,9 +55,9 @@ describe User do
 
   context "Validaçoes de endereço:" do
 
-    it "Não deve validar um logradouro permanente vazio" do
+    it "Deve validar um logradouro permanente vazio" do
       @user = User.new(:logradouro_permanente => '')
-      @user.should have_at_least(1).errors_on(:logradouro_permanente)
+      @user.should have(:no).errors_on(:logradouro_permanente)
     end
 
     it "Não deve validar um logradouro permanente com caracteres estranhos" do
@@ -65,13 +65,12 @@ describe User do
       @user.should have_at_least(1).errors_on(:logradouro_permanente)
     end
 
-    it "Não deve validar um número permanente vazio" do
+    it "Ddeve validar um número permanente vazio" do
       @user = User.new(:numero_permanente => '')
-      @user.should have_at_least(1).errors_on(:numero_permanente)
+      @user.should have(:no).errors_on(:numero_permanente)
     end
 
     it "Deve validar códigos postais vazios" do
-      pending "ARRUMAR!!!"
       @user = User.new(:cep_permanente => '')
       @user.should have(:no).errors_on(:cep_permanente)
     end
@@ -79,9 +78,11 @@ describe User do
     it "Deve validar códigos postais que possuam apenas números" do
       @user = User.new(:cep_permanente => '1234567890')
       @user.should have(:no).errors_on(:cep_permanente)
+    end
 
-      @user = User.new(:cep_permanente => 'a1234567890')
-      @user.should have_at_least(1).errors_on(:cep_permanente)
+    it "Deve validar códigos postais que possuam números e traços" do
+      @user = User.new(:cep_permanente => '12345-678')
+      @user.should have(:no).errors_on(:cep_permanente)
     end
 
     it "Deve validar um número permanente que seja formado por números e letras" do
@@ -95,19 +96,19 @@ describe User do
       @user.should have(:no).errors_on(:numero_permanente)
     end
 
-    it "Não deve validar uma cidade permanente vazio" do
+    it "Deve validar uma cidade permanente vazio" do
       @user = User.new(:cidade_permanente => '')
-      @user.should have_at_least(1).errors_on(:cidade_permanente)
+      @user.should have(:no).errors_on(:cidade_permanente)
     end
 
-    it "Não deve validar um estado permanente vazio" do
+    it "Deve validar um estado permanente vazio" do
       @user = User.new(:estado_permanente => '')
-      @user.should have_at_least(1).errors_on(:estado_permanente)
+      @user.should have(:no).errors_on(:estado_permanente)
     end
 
-    it "Não deve validar um país permanente vazio" do
+    it "Deve validar um país permanente vazio" do
       @user = User.new(:pais_permanente => '')
-      @user.should have_at_least(1).errors_on(:pais_permanente)
+      @user.should have(:no).errors_on(:pais_permanente)
     end
 
   end
