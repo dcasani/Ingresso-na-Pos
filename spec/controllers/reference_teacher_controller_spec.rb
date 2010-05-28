22b25ebc7d50abbf3818c125c70c3ef3a156b197
cassia <catitahtinha@gmail.com>
2010-05-28 17:14:58 -0300

    Inicializando testes de unidade para controller do Subscription para testar a estrutura de recomendar professores. por Cassia e Daniel

---
+++ spec/controllers/reference_teacher_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ReferenceTeachersController do
+
+  #Delete this example and add some real ones
+  it "should use ReferenceTeacherController" do
+    controller.should be_an_instance_of(ReferenceTeachersController)
+  end
+
+end
---
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe SubscriptionsController do
+
+  #Delete this example and add some real ones
+  it "should use SubscriptionsController" do
+    controller.should be_an_instance_of(SubscriptionsController)
+  end
+
+  it "Deve ser possivel recomendar um único professor" do
+   pending
+  end
+
+  it "Deve ser possível recomendar dois professores" do
+    pending
+  end
+
+end
