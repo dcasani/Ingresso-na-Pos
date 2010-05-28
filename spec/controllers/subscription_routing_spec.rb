4ccbc36c842d299d08c539866f58abde0f8e6d85
Silvia Esparrachiari <silviaeg@ime.usp.br>
2010-05-28 14:08:00 -0300

    Terminados testes de rota para a entidade Subscription.

--- spec/controllers/subscription_routing_spec.rb
+++ spec/controllers/subscription_routing_spec.rb
@@ -5,29 +5,46 @@ require 'spec_helper'
 
 describe SubscriptionsController do
 
-  it "should map /users/1/subscriptions to SubscriptionsController with new action" do
+  it "should map SubscriptionsController::NEW to /user/1/subscriptions/new" do
     route_for(:controller => 'subscriptions', :action => 'new', :user_id => '1').should == {:method => :get, :path => '/users/1/subscriptions/new'}
 
     params_from(:get, '/users/1/subscriptions/new').should == {:controller => 'subscriptions', :action => 'new', :user_id => '1'}
   end
 
-  it "should map 1 identified subscription from user 1 to its url home" do
-    route_for(:action => 'show', :controller => 'subscriptions', :user_id => '1', :id => '1').should == '/users/1/subscriptions/1'
-
-    params_from(:get, '/users/1/subscriptions/1').should == {:controller => 'subscriptions', :action => 'show', :user_id => '1', :id => '1'}
-  end
-
-  it "should map /users/1/subscriptions/1/edit to SubscriptionsController with edit action" do
-    route_for(:controller => 'subscriptions', :action => 'edit', :user_id => '1', :id => '1').should == {:method => :get , :path => '/users/1/subscriptions/1/edit'}
-
-    params_from(:get, '/users/1/subscriptions/1/edit').should == {:controller => 'subscriptions', :action => 'edit', :user_id => '1', :id => '1'}
-  end
-
-  it "should map /users/1/subscriptions with method POST to SubscriptionsController with create action" do
+  it "should map SubscriptionsController::CREATE to /user/1/subscriptions" do
     route_for(:controller => 'subscriptions', :action => 'create', :user_id => '1').should == {:method => :post , :path => '/users/1/subscriptions'}
 
     params_from(:post, '/users/1/subscriptions').should == {:controller => 'subscriptions', :action => 'create', :user_id => '1'}
   end
 
+  it "should map SubscriptionsController::DESTROY to /user/1/subscriptions/1" do
+    route_for(:controller => 'subscriptions', :action => 'destroy', :user_id => '1', :id => '1').should == {:method => :delete , :path => '/users/1/subscriptions/1'}
+
+    params_from(:delete, '/users/1/subscriptions/1').should == {:controller => 'subscriptions', :action => 'destroy', :user_id => '1', :id => '1'}
+  end
+
+  it "should map SubscriptionsController::UPDATE to /user/1/subscriptions/1" do
+    route_for(:controller => 'subscriptions', :action => 'update', :user_id => '1', :id => '1').should == {:method => :put , :path => '/users/1/subscriptions/1'}
+
+    params_from(:put, '/users/1/subscriptions/1').should == {:controller => 'subscriptions', :action => 'update', :user_id => '1', :id => '1'}
+  end
+
+  it "should map SubscriptionsController::SHOW to /user/1/subscriptions/1" do
+    route_for(:controller => 'subscriptions', :action => 'show', :user_id => '1', :id => '1').should == '/users/1/subscriptions/1'
+
+    params_from(:get, '/users/1/subscriptions/1').should == {:controller => 'subscriptions', :action => 'show', :user_id => '1', :id => '1'}
+  end
+
+  it "should map SubscriptionsController::EDIT to /user/1/subscriptions/1/edit" do
+    route_for(:controller => 'subscriptions', :action => 'edit', :user_id => '1', :id => '1').should == {:method => :get , :path => '/users/1/subscriptions/1/edit'}
+
+    params_from(:get, '/users/1/subscriptions/1/edit').should == {:controller => 'subscriptions', :action => 'edit', :user_id => '1', :id => '1'}
+  end
+
+  it "should map SubscriptionsController::INDEX to /user/1/subscriptions" do
+    route_for(:controller => 'subscriptions', :action => 'index', :user_id => '1').should == {:method => :get , :path => '/users/1/subscriptions'}
+
+    params_from(:get, '/users/1/subscriptions').should == {:controller => 'subscriptions', :action => 'index', :user_id => '1'}
+  end
 end
 
