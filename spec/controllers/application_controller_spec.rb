34aa1c089138ee81b7c386f2fb21a7f8cc11ce9d
cassia <catitahtinha@gmail.com>
2010-06-18 12:15:02 -0300

    Adicionando arquivos para testar a Application. Cassia

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ApplicationController do
+
+  #Delete this example and add some real ones
+  it "should use ApplicationController" do
+    controller.should be_an_instance_of(ApplicationController)
+  end
+
+end
